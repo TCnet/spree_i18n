f7898e30262ff124979aa7a244c8ad99d7796c9c
Hxmusic <hxmusic@foxmail.com>
2019-12-03 17:47:23 +0800

    update override

--- app/overrides/spree/adds_params_to_body.rb
+++ app/overrides/spree/adds_params_to_body.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(
-  virtual_path: 'spree/layouts/dislu',
+  virtual_path: 'spree/layouts/spree_application',
   name: 'adds_js_params',
   insert_top: 'body',
   partial: 'spree/layouts/add_js_params'
--- lib/spree_i18n/version.rb
+++ lib/spree_i18n/version.rb
@@ -10,7 +10,7 @@ module SpreeI18n
   module VERSION
     MAJOR = 3
     MINOR = 3
-    TINY  = 2
+    TINY  = 3
 
     STRING = [MAJOR, MINOR, TINY].compact.join('.')
   end
